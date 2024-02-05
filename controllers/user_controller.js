de784b593754ce15300549aeca05045b0c49c6b2
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-02-05 23:52:42 +0530

    set up controller

---
+++ controllers/user_controller.js
@@ -0,0 +1,4 @@
+module.exports.profile = (req, res)=>{
+    return res.end('<h1>User profile page</h1>');
+    
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 //create router instance
 const router = express.Router();
 
-console.log("Router is loaded");
+//require controller
+const homeController = require('../controllers/home_controller');
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
 module.exports = router;
