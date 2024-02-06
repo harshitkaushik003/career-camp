6b576c50b61af81fef34afc43e63b953843a4a91
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-02-06 23:32:34 +0530

    Added create session

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -39,3 +39,29 @@ module.exports.signIn = (req,res)=>{
         title: "signin"
     });
 }
+
+module.exports.createSession = (req,res)=>{
+    //find user
+    User.findOne({email: req.body.email})
+    .then((user)=>{
+        //handle user found
+        if(user){
+            //handle mismatching password
+            if(user.password != req.body.password){
+                console.log("passwords mismatching");
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id', user.id);
+            console.log("logged in");
+            return res.redirect('/users/profile');
+        }
+        //handle user not found
+        console.log("not found");
+        return res.redirect('/users/sign-up');
+    }).catch((err)=>{
+        console.log(`Error in finding the user ${err}`);
+    })
+
+
+}
