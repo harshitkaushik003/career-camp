298b8663b240e9a9fe7c73f2ec8421e14acb54f2
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-02-06 17:46:33 +0530

    setup views

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,6 @@
 module.exports.profile = (req, res)=>{
-    return res.end('<h1>User profile page</h1>');
+    return res.render('user', {
+        title: 'profile'
+    });
     
 }
--- index.js
+++ index.js
@@ -7,8 +7,23 @@ const app = express();
 //create port
 const port = 8000;
 
+//set up static files
+app.use(express.static('./assets'));
+
+//load layouts
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+
+//extract scripts and styles
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 //use express router
 app.use('/', require('./routes'));
+
+//set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 //fireup server
 app.listen(port, function(err){
