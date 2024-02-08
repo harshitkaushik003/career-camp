ad94f77efd6433937b61ade7f0ad688028dbc3e3
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-02-08 18:10:46 +0530

    added comments:

---
+++ controllers/comments_controller.js
@@ -0,0 +1,21 @@
+const Comment = require("../models/comments")
+const Post = require("../models/post")
+
+module.exports.createComments = (req, res)=>{
+    Post.findById(req.body.post)
+    .then((post)=>{
+        Comment.create(
+            {
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            }
+        ).then((comment)=>{
+            post.comments.push(comment);
+            post.save();
+        })
+    })
+
+    return res.redirect('/');
+}
+
--- models/post.js
+++ models/post.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema(
     {
         content: {type: String, required: true},
-        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        comments: [
+            {type: mongoose.Schema.Types.ObjectId, ref:'Comment'}
+        ]
     },{timestamps:true}
 )
 
